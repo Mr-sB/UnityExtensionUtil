3b0c7142b37e046d775e85ec5365e43e529f5997
GH-ZJ <wustzhangjie@gmail.com>
2020-07-06 16:04:09 +0800

    Add StringExtensions

---
+++ StringExtensions.cs
@@ -0,0 +1,30 @@
+namespace GameUtil.Extensions
+{
+    public static class StringExtensions
+    {
+        private static string GetText(this string key)
+        {
+            return key.Replace("\\n","\n").Replace("，", ",");
+        }
+        
+        public static string GetText(this string key, object arg0)
+        {
+            return string.Format(GetText(key), arg0);
+        }
+        
+        public static string GetText(this string key, object arg0, object arg1)
+        {
+            return string.Format(GetText(key), arg0, arg1);
+        }
+        
+        public static string GetText(this string key, object arg0, object arg1, object arg2)
+        {
+            return string.Format(GetText(key), arg0, arg1, arg2);
+        }
+        
+        public static string GetText(this string key, params object[] args)
+        {
+            return string.Format(GetText(key), args);
+        }
+    }
+}
